609616ec1ab29d4013690da5f8ba90be55b39582
kev5 <kevalkhara95@gmail.com>
2018-05-02 02:02:47 -0400

    update

--- neural-net-keras.py
+++ neural-net-keras.py
@@ -1,4 +1,5 @@
 from getEmbeddings import getEmbeddings
+import matplotlib.pyplot as plt
 import numpy as np
 import keras
 from keras import backend as K
@@ -8,13 +9,20 @@ from keras.layers import Dense, Dropout, LSTM, Embedding, Input, RepeatVector
 from keras.optimizers import SGD
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+import scikitplot.plotters as skplt
+
+
+def plot_cmat(yte, ypred):
+    '''Plotting confusion matrix'''
+    skplt.plot_confusion_matrix(yte, ypred)
+    plt.show()
 
 
 xtr,xte,ytr,yte = getEmbeddings("datasets/train.csv")
-np.save('./xtr',xtr)
-np.save('./xte',xte)
-np.save('./ytr',ytr)
-np.save('./yte',yte)
+np.save('./xtr', xtr)
+np.save('./xte', xte)
+np.save('./ytr', ytr)
+np.save('./yte', yte)
 
 xtr = np.load('./xtr.npy')
 xte = np.load('./xte.npy')
@@ -35,7 +43,7 @@ def baseline_model():
     # gradient descent
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     
-    # not 100% sure what "compile" does, maybe just runs the gradient descent algorithm
+    # configure the learning process of the model
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return model
 
@@ -52,4 +60,9 @@ estimator = model.fit(x_train, encoded_y, epochs=20, batch_size=64)
 print("Model Trained!")
 score = model.evaluate(x_test, encoded_y_test)
 print("")
-print("Accuracy = " + format(score[1]*100, '.2f') + "%")   # 92.62%
+print("Accuracy = " + format(score[1]*100, '.2f') + "%")   # 92.69%
+
+probabs = model.predict_proba(x_test)
+y_pred = np.argmax(probabs, axis=1)
+ 
+plot_cmat(y_test, y_pred)
